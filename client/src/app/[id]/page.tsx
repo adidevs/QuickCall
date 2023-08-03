9e6818c6878e79516bfce3bed292de05d5cfa75e
Aditya Sharma <99022811+adidevs@users.noreply.github.com>
2023-08-03 10:46:38 +0530

    Package Optimization

--- client/src/app/[id]/page.tsx
+++ client/src/app/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SocketContext } from '../SocketContext';
 import { useRouter } from "next/navigation";
 import styles from './Call.module.css';
@@ -8,32 +8,63 @@ export default function Call() {
 
 
   const router = useRouter();
+  const [isMuted, setIsMuted] = useState(false);
   const { localVideoRef, remoteVideoRef, peerId, sendMessage, connect, disconnect, isConnected } = useContext(SocketContext);
 
   useEffect(() => {
 
-      navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-        .then((localStream: MediaStream) => {
-          localVideoRef.current.srcObject = localStream;
-        })
-        .catch((err: any) => {
-          console.log('Failed to get local stream', err);
-        });
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+      .then((localStream: MediaStream) => {
+        localVideoRef.current.srcObject = localStream;
+      })
+      .catch((err: any) => {
+        console.log('Failed to get local stream', err);
+    });
 
-
+    window.onbeforeunload = () => {
+      stopLocalStream();
+    }
+    
     connect();
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const stopLocalStream = () => {
+    const localVideo = localVideoRef.current;
+    const localStream = localVideo.srcObject as MediaStream;
+    const tracks = localStream.getTracks();
+
+    tracks.forEach((track: MediaStreamTrack) => {
+      track.stop();
+    });
+
+    localVideo.srcObject = null;
+  }
 
   const hangUp = () => {
     disconnect();
+    stopLocalStream();
     router.push('/');
   }
 
+  const toggleAudio = () => {
+    setIsMuted(!isMuted);
+    const localVideo = localVideoRef.current;
+    const localStream = localVideo.srcObject as MediaStream;
+    const tracks = localStream.getTracks();
+
+    tracks.forEach((track: MediaStreamTrack) => {
+      if (track.kind === 'audio') {
+        track.enabled = !track.enabled;
+      }
+    });
+  }
+  
+
 
   return (
-    <div className="Home"> 
+    <div className="Home">
       <h1>Video Call</h1>
       <div >
         <span className="Local">
@@ -45,8 +76,8 @@ export default function Call() {
       </div>
       <p>Peer Id: {peerId}</p>
       <div className="RoomOptions">
-        <button>Mute</button>
         <button>Video</button>
+        <button onClick={toggleAudio}>{(isMuted)?"UnMute":"Mute"}</button>
         <button onClick={hangUp}>Hang Up</button>
       </div>
     </div>
